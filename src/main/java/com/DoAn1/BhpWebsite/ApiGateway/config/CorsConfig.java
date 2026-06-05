ee9c065ac59423c779535e2ae4cbf56249bb8efd
PhDuy2005 <kduy1706@gmail.com>
2026-06-05 22:02:39 +0700

    last push for gateway: final check for predicate, cors config, max upload file size

--- src/main/java/com/DoAn1/BhpWebsite/ApiGateway/config/CorsConfig.java
+++ src/main/java/com/DoAn1/BhpWebsite/ApiGateway/config/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				.allowedOrigins("http://localhost:3000")
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
+				.exposedHeaders("Content-Disposition", "Content-Length")
 				.allowCredentials(true)
 				.maxAge(3600);
 	}
